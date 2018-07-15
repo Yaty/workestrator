c575fc1babbf09a258e3e4c6e5b93d8bd167602c
Hugo Da Roit <contact@hdaroit.fr>
2018-07-16 00:10:57 +0200

    Remove dist file

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Farm from "./Farm";
-import { FarmOptions } from "./types";
-export declare function create(options: FarmOptions): Farm;
-export declare function kill(): Promise<void>;
-export default create;
