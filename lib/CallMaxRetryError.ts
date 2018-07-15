e50508370fed74ea21ef45bb0e225eec52e96424
Hugo Da Roit <contact@hdaroit.fr>
2018-07-15 15:12:49 +0200

    Rename err to reason

--- lib/CallMaxRetryError.ts
+++ lib/CallMaxRetryError.ts
@@ -1,5 +1,5 @@
 export default class CallMaxRetryError extends Error {
-    constructor(public err: Error, message: string = "Call max retried.") {
+    constructor(public reason: Error, message: string = "Call max retried.") {
         super(message);
         this.name = this.constructor.name;
         Error.captureStackTrace(this, this.constructor);
