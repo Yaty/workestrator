bdf96bac8371cc2f3cb6e912a0bc4cc1f9beeda7
Hugo Da Roit <contact@hdaroit.fr>
2018-07-19 00:28:13 +0200

    Test that --inspect and --debug is not inherited in a worker

--- test/fork.ts
+++ test/fork.ts
@@ -30,12 +30,32 @@ describe("Fork", () => {
         const res = await farm.runMethod("data");
         expect(res.cwd).to.equal(cwd);
         expect(res.env.foo).to.equal(env.foo);
-        expect(res.execArgv).to.deep.equal(execArgv);
+        expect(res.execArgv).to.include("--expose-gc");
+        expect(res.execArgv).to.include("--harmony");
         expect(res.execPath).to.deep.equal(execPath);
         expect(res.argv).to.deep.equal([
             process.execPath,
             path.resolve(__dirname, "../lib/worker/executor.js"),
             ...args,
         ]);
+    });
+
+    it("should remove debug and inspect from execArgv", async () => {
+        try {
+            process.execArgv.push("--debug", "--inspect");
+
+            const farm = create({
+                module: childPath,
+            });
+
+            const res = await farm.runMethod("data");
+            expect(res.execArgv).to.not.include("--debug");
+            expect(res.execArgv).to.not.include("--inspect");
+        } catch (err) {
+            throw err;
+        } finally {
+            process.execArgv.splice(process.execArgv.findIndex((a) => a === "--debug"), 1);
+            process.execArgv.splice(process.execArgv.findIndex((a) => a === "--inspect"), 1);
+        }
     });
 });
