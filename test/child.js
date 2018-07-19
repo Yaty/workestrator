5e791fbb2931df880ab14063057f00d932545103
Hugo Da Roit <contact@hdaroit.fr>
2018-07-19 21:04:34 +0200

    Remove useless methods

--- test/child.js
+++ test/child.js
@@ -1,7 +1,4 @@
-const fs = require("fs");
-const started = Date.now();
-
-module.exports = async function(...args) {
+module.exports = function(...args) {
     return new Promise((resolve) => {
         const data = {
             pid: process.pid,
@@ -36,23 +33,6 @@ module.exports.run0 = async function() {
     return await module.exports(0);
 };
 
-module.exports.std = function() {
-    process.stdout.write("stdout\n");
-    process.stderr.write("stderr\n");
-    // process.stdin.write("stdin");
-};
-
-module.exports.killable = function(id) {
-    if (Math.random() < 0.5) {
-        return process.exit(-1);
-    }
-
-    return {
-        id,
-        pid: process.pid,
-    };
-};
-
 module.exports.err = function(type, message) {
     switch (type) {
         case "TypeError": throw new TypeError(message);
@@ -69,32 +49,10 @@ module.exports.block = function() {
     while (true) {}
 };
 
-// use provided file path to save retries count among terminated workers
-module.exports.stubborn = function(path) {
-    function isOutdated(p) {
-        return ((new Date()).getTime() - fs.statSync(p).mtime.getTime()) > 2000;
-    }
-
-    // file may not be properly deleted, check if modified no earler than two seconds ago
-    if (!fs.existsSync(path) || isOutdated(path)) {
-        fs.writeFileSync(path, "1");
-        process.exit(-1);
-    }
-
-    const retry = parseInt(fs.readFileSync(path, "utf8"), 10);
-
-    if (Number.isNaN(retry)) {
-        throw new Error("file contents is not a number");
-    }
-
-    if (retry > 4) {
-        return 12;
-    } else {
-        fs.writeFileSync(path, String(retry + 1));
-        process.exit(-1);
-    }
-};
-
-module.exports.uptime = function() {
-   return Date.now() - started;
+module.exports.failTimeout = function(timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error());
+        }, timeout || 500);
+    });
 };
