e01ada96f060ac105e7f89883587f3df301bf65c
Hugo Da Roit <contact@hdaroit.fr>
2018-07-23 23:42:04 +0200

    Add benchmarks for serialization

---
+++ benchmarks/serialization/README.md
@@ -0,0 +1,5 @@
+# CBOR
+
+CBOR is at least 4x times slower than the JSON serializer.
+However it can serialize much more data types.
+Run `node benchmarks/serialization` to see results for simple data types.
---
+++ benchmarks/serialization/index.js
@@ -0,0 +1,76 @@
+const workhorse = require("../../dist");
+
+const farmsOptions = {
+    numberOfWorkers: 1,
+    module: require.resolve("./child"),
+    maxConcurrentCallsPerWorker: 1,
+    maxRetries: 0,
+};
+
+function waitForWorkersToLoad(farm) {
+    return new Promise((resolve) => {
+        const itv = setInterval(() => {
+            const readyWorkers = farm.workers.reduce((acc, w) => acc + Number(w.isAvailable()), 0);
+
+            if (readyWorkers === farm.workers.length) {
+                clearInterval(itv);
+                resolve();
+            }
+        }, 50);
+    });
+}
+
+const jsonFarm = workhorse.create({
+    ...farmsOptions,
+    serializerPath: workhorse.Serializers.JSON,
+});
+
+const cborFarm = workhorse.create({
+    ...farmsOptions,
+    serializerPath: workhorse.Serializers.CBOR,
+});
+
+const iterations = 500;
+
+async function run(farm) {
+    const calls = [];
+    const data = {
+        number: 1,
+        string: "",
+        object: {
+            a: 1,
+        },
+        null: null,
+        array: [1, "2", 3],
+    };
+
+    const startTime = process.hrtime();
+
+    for (let i = 0; i < iterations; i++) {
+        calls.push(farm.run(data))
+    }
+
+    await Promise.all(calls);
+
+    const endTime = process.hrtime(startTime);
+    const diff = (endTime[0] * 1000) + (endTime[1] / 1000000);
+    console.log("Done in", diff, "ms");
+}
+
+/*
+For this benchmarks we compare simple data types as:
+- Number
+- String
+- Object (with null)
+- Array
+ */
+(async () => {
+    await waitForWorkersToLoad(jsonFarm);
+    console.log("JSON Serializer :");
+    await run(jsonFarm);
+
+    await waitForWorkersToLoad(cborFarm);
+    console.log("\nCBOR Serializer :");
+    await run(cborFarm);
+    process.exit(0);
+})();
